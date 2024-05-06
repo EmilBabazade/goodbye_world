7e6f6b3974d71f86109c0ab928a9e84609616500
Emil <emilbabazade29@gmail.com>
2024-05-06 21:33:11 +0300

    error output and its test

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,7 +1,13 @@
 use assert_cmd::Command;
 
 #[test]
-fn works() {
+fn goodbye_world_works() {
     let mut command = Command::cargo_bin("goodbye_world").unwrap();
     command.assert().success().stdout("Goodbye, world!\n");
+}
+
+#[test]
+fn error_output_works() {
+    let mut command = Command::cargo_bin("error_output").unwrap();
+    command.assert().success().stderr("oopsie!\n");
 }
